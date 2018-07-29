e7b53c7a2b4f5e2a6c5f6f8ed556fd59bd8d87d3
duong bao tran <duongbaotran224@gmail.com>
2018-07-29 12:54:57 +0700

    test animation

--- src/Web.jsx
+++ src/Web.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
+
+const styles ={
+    "root": {
+        backgroundColor: 'red',
+        animationName: 'fading',
+        animationDuration: '4s' 
+    },
+    '@keyframes fading': {
+        from: {
+            width: '0%',
+            opacity: 0
+        },
+        to: {
+            width: '100%',
+            opacity: 1
+        }
+    }
+}
 
 class Web extends React.Component {
     render() {
+        const {classes} = this.props;
         return (
-            <div>'ABC'</div>
+            <div className={classes.root}>'ABC'</div>
         )
     }
 }
-export default Web
+Web.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Web)
